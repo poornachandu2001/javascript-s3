43e121738949109587778bb98772194f0b7332cd
poornachandu2001 <99313312+poornachandu2001@users.noreply.github.com>
2022-03-16 17:21:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,203 @@
+function coach(seatn, row)
+{
+    if(row == 1)
+    {
+        if(seatn == 1 || seatn == 4)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 2 || seatn == 5)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 3 || seatn == 6)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 7)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 2)
+    {
+        if(seatn == 9 || seatn == 12)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 10 || seatn == 13)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 11 || seatn == 14)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 15)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 3)
+    {
+        if(seatn == 17 || seatn == 20)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 18 || seatn == 21)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 19 || seatn == 22)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 23)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 4)
+    {
+        if(seatn == 25 || seatn == 28)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 26 || seatn == 29)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 27 || seatn == 30)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 31)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 5)
+    {
+        if(seatn == 33 || seatn == 36)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 34 || seatn == 37)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 35 || seatn == 38)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 39)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 6)
+    {
+        if(seatn == 41 || seatn == 44)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 42 || seatn == 45)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 43 || seatn == 46)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 47)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 7)
+    {
+        if(seatn == 49 || seatn == 52)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 50 || seatn == 53)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 51 || seatn == 54)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 55)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else if(row == 8)
+    {
+        if(seatn == 57 || seatn == 60)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 58 || seatn == 61)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 59 || seatn == 62)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 63)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }
+    else
+    {
+        if(seatn == 65 || seatn == 68)
+        {
+            return "Your seat is:lower berth"
+        }
+        else if(seatn == 66 || seatn == 69)
+        {
+            return "Your seat is:middle berth"
+        }
+        else if(seatn == 67 || seatn == 70)
+        {
+            return "Your seat is:upper berth"
+        }
+        else if(seatn == 71)
+        {
+            return "Your seat is:side lower berth"
+        }
+        else{
+            return "Your seat is:side upper berth"
+        }
+    }   
+}
+console.log(coach(72,9))
+console.log(coach(54,7))
